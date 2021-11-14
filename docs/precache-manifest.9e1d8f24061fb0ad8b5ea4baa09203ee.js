f08d82ed673deda1b757b95baade735ace1b0329
Pedro Augusto <pedro42augusto@gmail.com>
2021-11-14 08:31:41 -0300

    Add to GH pages

---
+++ docs/precache-manifest.9e1d8f24061fb0ad8b5ea4baa09203ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54eed754a4dc3a5d886f5354041fdc78",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a272e07ed6fb3bc7611",
+    "url": "./static/css/main.be2719f9.chunk.css"
+  },
+  {
+    "revision": "66bd0770cae41e9bd27b",
+    "url": "./static/js/2.5f6c3b66.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5f6c3b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a272e07ed6fb3bc7611",
+    "url": "./static/js/main.5067241b.chunk.js"
+  },
+  {
+    "revision": "e7dd1b16019095e417f5",
+    "url": "./static/js/runtime-main.fa4642c0.js"
+  }
+]);
